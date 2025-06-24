3f0dbf54140e71a1d3b0f9995d6b8d6c6a673527
Yash Bhardwaj <yash.bhardwaj@yashss481.successivetech.com>
2025-06-24 16:02:23 +0530

    fix: fixed the code structure

---
+++ src/Components/Assignment1Components/TaskList.jsx
@@ -0,0 +1,18 @@
+// 5.Create a functional component named TaskList that accepts an array of task names as a prop.
+// Use the map function to render each task name as a list item.
+// Import and render the TaskList component in the App component with an array of tasks.
+
+import React from "react";
+
+export default function TaskList({ List }) {
+  return (
+    <div>
+      <h2>My Tasks are:</h2>
+      <ul>
+        {List.map((task, index) => (
+          <li key={index}>{task}</li> // ✅ implicit return
+        ))}
+      </ul>
+    </div>
+  );
+}
